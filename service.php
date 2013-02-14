e1a120f20f4c23fbfba4e07d22c1cececeb28159
Zak Angelle <zaknotzak@gmail.com>
2013-02-14 13:47:42 -0600

    Updated shell variables to be more generic

--- service.php
+++ service.php
@@ -4,28 +4,28 @@
   if(isset($_GET["user"]) && intval($_GET["user"])) {
 
     /* Soak in the passed variable or set our own */
-    $number_of_posts = isset($_GET["num"]) ? intval($_GET["num"]) : 10; // 10 is default
+    $number_of_items = isset($_GET["num"]) ? intval($_GET["num"]) : 10; // 10 is default
     $user_id = intval($_GET["user"]); // no default
 
     /* Connect to the DB */
     $connect = mysql_connect("localhost", "username", "password") or die("Cannot connect to the DB");
     mysql_select_db("db_name", $connect) or die("Cannot select the DB");
 
-    /* Grab the posts from the DB */
-    $query = "SELECT post_title, guid FROM wp_posts WHERE post_author = $user_id AND post_status = 'publish' ORDER BY ID DESC LIMIT $number_of_posts";
+    /* Grab the items from the DB */
+    $query = "SELECT column_1, column_2 FROM table_name WHERE columns_1 = $user_id ORDER BY ID DESC LIMIT $number_of_items";
     $result = mysql_query($query, $connect) or die("Errant query: ".$query);
 
     /* Create one master array of the records */
-    $posts = array();
+    $items = array();
     if(mysql_num_rows($result)) {
-      while($post = mysql_fetch_assoc($result)) {
-        $posts[] = array("post" => $post);
+      while($item = mysql_fetch_assoc($result)) {
+        $items[] = array("item" => $item);
       }
     }
 
     /* Output in JSON format */
     header("Content-type: application/json");
-    echo json_encode(array("posts" => $posts));
+    echo json_encode(array("items" => $items));
 
     /* Disconnect from the DB */
     @mysql_close($connect);
